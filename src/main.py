fcd7cce74554b24272ac7280ab205b7a4b9c85ec
tyommmm <artyomhovsepyan135@gmail.com>
2024-03-06 21:42:39 +0400

    Removed bw_scanner.

--- src/main.py
+++ src/main.py
@@ -128,23 +128,23 @@ plt.figure(figsize=(16, 10))
 plt.imshow(scanned)
 
 
-def bw_scanner(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    T = threshold_local(gray, 21, offset=5, method="gaussian")
-    return (gray > T).astype("uint8") * 255
+# def bw_scanner(image):
+#     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#     T = threshold_local(gray, 21, offset=5, method="gaussian")
+#     return (gray > T).astype("uint8") * 255
 
 
-result = bw_scanner(scanned)
-plot_gray(result)
+# result = bw_scanner(scanned)
+plot_gray(scanned)
 
 RESULT_IMAGE_PATH = "./src/images/result.png"
-output = Image.fromarray(result)
+output = Image.fromarray(scanned)
 output.save(RESULT_IMAGE_PATH)
 
 
 img = Image.open(RESULT_IMAGE_PATH)
 
-text = pytesseract.image_to_string(img, lang="Armenian+rus+osd")
+text = pytesseract.image_to_string(img, lang="Armenian+rus")
 
 print(text)
 
