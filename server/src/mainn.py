400f483fbefe2ab8d704d70c389e6c85abb71c03
Vahag <sargsyanvahag6@gmail.com>
2024-03-29 14:48:46 +0400

    Add support for processing multiple images

--- server/src/mainn.py
+++ server/src/mainn.py
@@ -1,6 +1,6 @@
-from authentication.authentication import app
+# from authentication.authentication import app
 
-# from client_image_processing.image_processing import app
+from client_image_processing.image_processing import app
 
 # from client_image_processing.add_purchases import app
 
